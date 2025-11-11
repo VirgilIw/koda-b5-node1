65dd6deb9a72ca533d936bea44c6646bb165355c
Virgil I Ambar <imanuelambar11@gmail.com>
2025-11-11 21:19:07 +0700

    feat: eslint, minitask, and add scripts in file package.json

--- node1.js
+++ node1.js
@@ -2,27 +2,26 @@ import { createInterface } from "node:readline/promises";
 import moment from "moment";
 
 import { stdin, stdout } from "node:process";
-const rl = createInterface({
+const rl = createInterface ({
   input: stdin,
   output: stdout,
 });
-
-const momentDate = async () => {
+async function momentDate() {
   try {
-    const answer = await rl.question("masukan tanggal, ex(DD-MM-YYYY): ");
+    const answer = await rl.question ("masukan tanggal, ex(DD-MM-YYYY): ");
     const formAwal = "DD-MM-YYYY";
-    const parseInput = moment(answer, formAwal, true);
+    const parseInput = moment (answer, formAwal, true);
 
-    if (parseInput.isValid()) {
-      console.log("tanggal valid", parseInput.format("DD/MM/YYYY"));
+    if (parseInput.isValid ()) {
+      console.log ("tanggal valid", parseInput.format ("DD/MM/YYYY"));
     } else {
-      console.log("format tanggal salah");
-      return momentDate();
+      console.log ("format tanggal salah");
+      return momentDate ();
     }
   } catch (err) {
-    console.log(err);
+    console.log (err);
   }
-  rl.close();
-};
+  rl.close ();
+}
 
-momentDate();
+momentDate ();
--- node2.js
+++ node2.js
@@ -1,43 +1,45 @@
 import { createInterface } from "node:readline/promises";
 
 import { stdin, stdout } from "node:process";
-const rl = createInterface({
+
+const rl = createInterface ({
   input: stdin,
   output: stdout,
 });
 
 const getDate = async () => {
-  let answer = await rl.question("masukan tanggal (DD-MM-YYYY): ");
+  let answer = await rl.question ("masukan tanggal (DD-MM-YYYY): ");
 
   if (
-    !answer.includes("-") ||
+    !answer.includes ("-") ||
     answer.length !== 10 ||
     answer[2] !== "-" ||
     answer[5] !== "-"
   ) {
-    console.log("format tanggal salah");
-    return getDate();
+    console.log ("format tanggal salah");
+    return getDate ();
   }
 
-  const day = Number(answer.slice(0, 2));
-  const month = Number(answer.slice(3, 5));
-  const year = Number(answer.slice(6));
+  const day = Number (answer.slice (0, 2));
+  const month = Number (answer.slice (3, 5));
+  const year = Number (answer.slice (6));
 
   if (
-    isNaN(day) ||
-    isNaN(month) ||
-    isNaN(year) ||
+    isNaN (day) ||
+    isNaN (month) ||
+    isNaN (year) ||
     day < 1 ||
     day > 31 ||
     month < 1 ||
     month > 12 ||
-    year.toString().length !== 4
+    year.toString ().length !== 4
   ) {
-    console.log("tanggal tidak valid");
-    return getDate();
+    console.log ("tanggal tidak valid");
+    return getDate ();
   }
 
   let newAnswer = "";
+
   for (let i = 0; i < answer.length; i++) {
     if (answer[i] === "-") {
       newAnswer = newAnswer + "/";
@@ -45,7 +47,7 @@ const getDate = async () => {
       newAnswer = newAnswer + answer[i];
     }
   }
-  console.log(newAnswer);
-  rl.close();
+  console.log (newAnswer);
+  rl.close ();
 };
-getDate();
+getDate ();
